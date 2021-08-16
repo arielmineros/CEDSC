ce63db354b4b1118c5b51a6559c69b191ca1dd86
arielmineros <bryangmineros@gmail.com>
2021-08-15 21:03:46 -0600

    cambio en comando cls

--- Menu.py
+++ Menu.py
@@ -4,7 +4,7 @@ import os
 # Creación del menú
 while True:
     #Limpieza de pantalla
-    #os.system("cls") ------> Descomentar cuando se usa windows
+    #os.system("cls") 
     os.system("clear")
     print("1. Añadir un C.E \n2. Ver todos los C.E. \n3. Actualizar un C.E. \n4. Eliminar un C.E. \n5. Salir")
     option = int(input("\nIngrese la opción: "))
