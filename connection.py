0f96872e7b391d9375c08fbd4902373f23c80364
Ariel Mineros <74280427+arielmineros@users.noreply.github.com>
2023-09-17 22:15:04 -0600

    Update connection.py
    asdf

--- connection.py
+++ connection.py
@@ -1,5 +1,5 @@
 import pymongo
 
-client = pymongo.MongoClient("mongodb+srv://arielgmineros:Ilikerock4213@cluster0.1mhey.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://arielgmineros:password@cluster0.1mhey.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 # String to connect to database on atlas
 # Import as "con"
